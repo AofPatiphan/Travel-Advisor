d6b28deb6039bdeff262b95844db2e09a8ad147a
AofPatiphan <patiphan.ma@hotmail.com>
2022-02-06 20:47:23 +0700

    save

---
+++ src/components/PlaceDetails/PlaceDetails.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function PlaceDetails() {
+    return (
+        <div>
+            <h1>This is PlaceDetail</h1>
+        </div>
+    );
+}
+
+export default PlaceDetails;
